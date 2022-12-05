99ba10fcd662d0ded54615fb79f3edce81515e91
SSong-develop <thdgnsrl123@naver.com>
2022-12-05 23:19:43 +0900

    adsfasd

--- app/src/main/java/com/ssong_develop/limitwidthlinearlayout/LimitWidthLinearLayout.kt
+++ app/src/main/java/com/ssong_develop/limitwidthlinearlayout/LimitWidthLinearLayout.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
 import android.widget.LinearLayout
+import com.google.android.material.textview.MaterialTextView
 import java.lang.Integer.max
 
 class LimitWidthLinearLayout @JvmOverloads constructor(
@@ -12,10 +13,13 @@ class LimitWidthLinearLayout @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
+    private val childViewSet = mutableSetOf<MaterialTextView>()
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         var maxWidth = 0
         for (i in 0 until childCount) {
             val child = getChildAt(i)
+            childViewSet.add(child as MaterialTextView)
             child.measure(
                 MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED),
                 MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
@@ -29,6 +33,9 @@ class LimitWidthLinearLayout @JvmOverloads constructor(
     }
 
     fun test() {
+        childViewSet.forEach {
+            Log.d("ssong-develop2","${it}")
+        }
         Log.d("ssong-develop1","Limit Function Invoke!")
     }
 }
